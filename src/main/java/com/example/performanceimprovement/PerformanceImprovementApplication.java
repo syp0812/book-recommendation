7543dc4cfe08ca9806840e8d4a4d6b67edc71cb7
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-05-19 23:40:20 +0900

    first commit

---
+++ src/main/java/com/example/performanceimprovement/PerformanceImprovementApplication.java
@@ -0,0 +1,13 @@
+package com.example.performanceimprovement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceImprovementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceImprovementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/performanceimprovement/PerformanceImprovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.performanceimprovement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceImprovementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
