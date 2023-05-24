c117def9902ba1ee8971ec7145958230922e8f0c
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-05-24 13:20:54 +0900

    chore: 서브 모듈을 생성하고 api, web, batch 모듈에 core 모듈 의존성을 추가한다. (#1)

---
+++ batch-module/src/main/java/com/example/batchmodule/BatchModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchModuleApplication.class, args);
+    }
+
+}
